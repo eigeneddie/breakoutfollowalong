98088e72ab1d709383659e23dca9a8ac7601cba1
edgar sutawika <edgar@reekon.tools>
2026-01-18 04:53:33 -0500

    now working, still wip

--- code/win32_platform.c
+++ code/win32_platform.c
@@ -1,9 +1,46 @@
-#include <stdio.h>
-
+#include <windows.h>
 // NOTE: 
 // to build program, run  call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 
 // on the cmd prompt so you can build the code from using cl command
 
+/*
 void main(){
     printf("hello sailor!\n");
+}
+*/
+
+int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
+{
+
+    WNDCLASSA window_class = {0};
+    window_class.style = CS_HREDRAW|CS_VREDRAW;
+    window_class.lpClassName = "Game_window_class";
+    WNDPROC   lpfnWndProc;
+    int       cbClsExtra;
+    int       cbWndExtra;
+    HINSTANCE hInstance;
+    HICON     hIcon;
+    HCURSOR   hCursor;
+    HBRUSH    hbrBackground;
+    LPCSTR    lpszMenuName;
+    LPCSTR    lpszClassName;
+
+    RegisterClassA(
+        const WNDCLASSA *lpWndClass
+        );
+
+    HWND window = CreateWindowExA(
+        DWORD     dwExStyle,
+        LPCSTR    lpClassName,
+        LPCSTR    lpWindowName,
+        DWORD     dwStyle,
+        int       X,
+        int       Y,
+        int       nWidth,
+        int       nHeight,
+        HWND      hWndParent,
+        HMENU     hMenu,
+        HINSTANCE hInstance,
+        LPVOID    lpParam
+        );
 }
