94608c7ab8e6a64cb45ae4935f03ea4b8c3798fa
edgar sutawika <edgar@reekon.tools>
2026-01-18 04:30:20 -0500

    hello world using exe file compiled by vsvarsall

---
+++ build/build.bat
@@ -0,0 +1,5 @@
+@echo off
+pushd ..\build
+cl -nologo -Zi ..\code\win32_platform.c
+
+popd
---
+++ code/win32_platform.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// NOTE: 
+// to build program, run  call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 
+// on the cmd prompt so you can build the code from using cl command
+
+void main(){
+    printf("hello sailor!\n");
+}
